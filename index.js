f7d72e0d41a0164036d7f1500f1ade5ab1a5a13b
Gavin <gavinvm@protonmail.com>
2025-04-28 17:43:42 +0200

    feat: Initial shop simulation with buggy code

--- index.js
+++ index.js
@@ -6,8 +6,38 @@ const menu = [
 ];
 
 const cashInRegister = 100;
+const nextOrderId = 1;
 const orderQueue = [];
 
-const addNewPizza = (pizzaObject) => {
-  menu.push(pizzaObject);
-};
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  cashInRegister += selectedPizza.price;
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: 'ordered',
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = 'completed';
+  return order;
+}
+
+addNewPizza({ name: 'Chicken Bacon Ranch', cost: 12 });
+addNewPizza({ name: 'BBQ Chicken', cost: 12 });
+addNewPizza({ name: 'Spicy Sausage', cost: 11 });
+
+placeOrder('Chicken Bacon Ranch');
+completeOrder('1');
+
+console.log('Menu:', menu);
+console.log('Cash in register:', cashInRegister);
+console.log('Order queue:', orderQueue);
