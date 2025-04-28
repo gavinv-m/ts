5ee30c6439184944e6dc0da2e998f6c65ea81c6b
Gavin <gavinvm@protonmail.com>
2025-04-28 19:48:02 +0200

    feat: Create a separate address type

--- person.ts
+++ person.ts
@@ -1,12 +1,18 @@
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
 type Person = {
   name: string;
   age: number;
   isStudent: boolean;
-  address: {
-    street: string;
-    city: string;
-    country: string;
-  };
+  address: Address;
 };
 
 let person1: Person = {
